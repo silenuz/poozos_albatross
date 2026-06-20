31a9da0cc302e839c013d92c499d5b62ecf77d4e
silenuznowan <horuuendillus@gmail.com>
2026-06-20 10:47:33 -0400

    organized module

--- src/spirare/caecias/class_doc_elements.py
+++ src/spirare/caecias/class_doc_elements.py
@@ -9,80 +9,12 @@
 @Author: Silenuz Nowan (silenuznowan@yahoo.com)
 """
 from __future__ import annotations
-
+from dataclasses import dataclass, field
 from .class_doc_base import *
 
 @dataclass(slots=True, kw_only=True)
-class ClassDocReturn(MemberQualifierBase):
-
-    type_value: None | str = field(
-        default=None,
-        metadata={
-            "name": "type",
-            "type": "Attribute",
-        },
-    )
-
-
-@dataclass(slots=True, kw_only=True)
-class ClassDocParameter(ClassDocReturn):
-
-    index: None | int = field(
-        default=None,
-        metadata={
-            "type": "Attribute",
-        },
-    )
-    name: None | str = field(
-        default=None,
-        metadata={
-            "type": "Attribute",
-        },
-    )
-
-    default: None | str = field(
-        default=None,
-        metadata={
-            "type": "Attribute",
-        },
-    )
-
-@dataclass(slots=True, kw_only=True)
-class ClassDocConstructor(MethodReturnBase):
-    pass
-
-@dataclass(slots=True, kw_only=True)
-class ClassDocOperator(MethodReturnBase):
-    pass
-
-
-@dataclass(slots=True, kw_only=True)
 class ClassDocAnnotation(MethodReturnKeyword):
-   pass
-
-@dataclass(slots=True, kw_only=True)
-class ClassDocSignal(MethodBase,MethodTagBase):
     pass
-
-@dataclass(slots=True, kw_only=True)
-class ClassDocReturnsError:
-
-    number: None | int = field(
-        default=None,
-        metadata={
-            "type": "Attribute",
-        },
-    )
-
-@dataclass(slots=True, kw_only=True)
-class ClassDocMethod(MethodReturnKeyword,MethodTagBase):
-
-    returns_error: list[ClassDocReturnsError] = field(
-        default_factory=list,
-        metadata={
-            "type": "Element",
-        },
-    )
 
 @dataclass(slots=True, kw_only=True)
 class ClassDocConstant(MemberBase, MemberQualifierBase):
@@ -96,26 +28,8 @@ class ClassDocConstant(MemberBase, MemberQualifierBase):
     )
 
 @dataclass(slots=True, kw_only=True)
-class ClassDocThemeItem(MemberBase):
-    data_type: None | str = field(
-        default=None,
-        metadata={
-            "type": "Attribute",
-        },
-    )
-    type_value: None | str = field(
-        default=None,
-        metadata={
-            "name": "type",
-            "type": "Attribute",
-        },
-    )
-    default: None | str = field(
-        default=None,
-        metadata={
-            "type": "Attribute",
-        },
-    )
+class ClassDocConstructor(MethodReturnBase):
+    pass
 
 
 @dataclass(slots=True, kw_only=True)
@@ -152,6 +66,78 @@ class ClassDocMember(ClassDocConstant):
             "type": "Attribute",
         },
     )
+
+
+@dataclass(slots=True, kw_only=True)
+class ClassDocMethod(MethodReturnKeyword,MethodTagBase):
+
+    returns_error: list[ClassDocReturnsError] = field(
+        default_factory=list,
+        metadata={
+            "type": "Element",
+        },
+    )
+
+
+@dataclass(slots=True, kw_only=True)
+class ClassDocOperator(MethodReturnBase):
+    pass
+
+@dataclass(slots=True, kw_only=True)
+class ClassDocParameter(MethodParameterBase):
+    pass
+
+
+@dataclass(slots=True, kw_only=True)
+class ClassDocReturn(MemberQualifierBase):
+
+    type_value: None | str = field(
+        default=None,
+        metadata={
+            "name": "type",
+            "type": "Attribute",
+        },
+    )
+
+
+@dataclass(slots=True, kw_only=True)
+class ClassDocReturnsError:
+
+    number: None | int = field(
+        default=None,
+        metadata={
+            "type": "Attribute",
+        },
+    )
+
+
+@dataclass(slots=True, kw_only=True)
+class ClassDocSignal(MethodBase,MethodTagBase):
+    pass
+
+
+@dataclass(slots=True, kw_only=True)
+class ClassDocThemeItem(MemberBase):
+    data_type: None | str = field(
+        default=None,
+        metadata={
+            "type": "Attribute",
+        },
+    )
+    type_value: None | str = field(
+        default=None,
+        metadata={
+            "name": "type",
+            "type": "Attribute",
+        },
+    )
+    default: None | str = field(
+        default=None,
+        metadata={
+            "type": "Attribute",
+        },
+    )
+
 
 @dataclass(slots=True, kw_only=True)
 class ClassTutorialsLink:
