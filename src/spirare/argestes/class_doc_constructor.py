0fc8ef9fbfa7e53fbfe96521a33c6668f2ee5545
silenuznowan <horuuendillus@gmail.com>
2026-07-06 13:38:55 -0400

    fixed missing return_value parameter in ClassDocConstructor init

--- src/spirare/argestes/class_doc_constructor.py
+++ src/spirare/argestes/class_doc_constructor.py
@@ -11,6 +11,7 @@
 from xml.etree.ElementTree import Element
 from xml.etree import ElementTree as Et
 
+from . import ClassDocReturn
 from .doc_base import MethodReturnBase, ModelCollection, DocParameters, DocDescription, Zucaritas
 
 
@@ -28,8 +29,10 @@ class ClassDocConstructor(MethodReturnBase,Zucaritas):
     """
     __slots__ = ()
 
-    def __init__(self, name:str, description:DocDescription=DocDescription(),qualifiers:str=None, parameters: DocParameters = None):
-        MethodReturnBase.__init__(self, name=name, description=description,qualifiers=qualifiers, parameters=parameters)
+    def __init__(self, name:str, description:DocDescription=DocDescription(),qualifiers:str=None,
+                 parameters: DocParameters = None,return_value: ClassDocReturn = None):
+        MethodReturnBase.__init__(self, name=name, description=description,qualifiers=qualifiers,
+                                  parameters=parameters, return_value=return_value)
 
 
     def to_xml_doc(self)->Element:
