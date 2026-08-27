b542e8152ef8f4ff5aafffe2823a81863e0ffa25
silenuznowan <horuuendillus@gmail.com>
2026-08-27 10:40:48 -0400

    started sample usage for lz

---
+++ bin/lz_sample.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Project: poozos_albatross
+@Date: 8/27/26
+@File: lz_sample
+
+@Author: Silenuz Nowan (silenuznowan@yahoo.com)
+"""
+import sys
+from pathlib import Path
+from spirare.luckys_zephyr import LuckyZephyr
+
+# Get the absolute path to this script
+script_path = Path(__file__).resolve()
+
+# Get the absolute path to the directory containing the python scripts
+spirare_script_dir = script_path.parent.parent / 'src' / 'spirare'
+sys.path.append(str(spirare_script_dir))
+
+# use generated doxygen for the Summator class in the example directory
+summator_doxy_class_xml =  script_path.parent.parent / 'example' / 'doxygen_output' / 'xml' / 'classSummator.xml'
+lz = LuckyZephyr(summator_doxy_class_xml)
+
+# get and print class brief
+brief_description = lz.get_class_brief()
+print(f'Brief Description:\n {brief_description}')
+
+# get and print class description:
+detailed_description = lz.get_class_detail()
+print(f'Detailed Description:\n {detailed_description}')
+
+# fields and methods return member definitions
+# look up constant value 'MINIMUM_REQUIRED_AMOUNT':
+# field can be looked up by name:
+member_definition = lz.get_definition_by_name('MINMUM_REQUIRED_AMOUNT')
+print(f'\nConstant Details:')
+print(f'Description: {member_definition.detaileddescription}')
+print(f'Type: {member_definition.type}')
+print(f'Initial Value: {member_definition.initializer_value}')
+
+# fields can also be looked up by qualified name:
+member_definition = lz.get_definition_by_qualified('Summator::DOING_OKAY_AMOUNT')
+print(f'\nConstant Details:')
+print(f'Description: {member_definition.detaileddescription}')
+print(f'Type: {member_definition.type}')
+print(f'Initial Value: {member_definition.initializer_value}')
